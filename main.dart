7bc69ad58e765a34d4bf1746c6dee27595abf05e
Kyuraaa27 <bacancity21@gmail.com>
2024-11-27 15:04:20 +0700

    BAB 4

--- main.dart
+++ main.dart
@@ -1,8 +1,11 @@
+import 'package:e_commerce/widget/BigCard.dart';
+import 'package:e_commerce/widget/ProductCard.dart';
+import 'package:e_commerce/widget/SectionHeader.dart';
 import 'package:flutter/material.dart';
-import 'package:bab5/pages/book_list_page.dart';
-import 'package:bab5/pages/add_book_page.dart';
-import 'package:bab5/pages/edit_book_page.dart';
-import 'package:bab5/pages/book_detail_page.dart';
+import 'widget/TopSeller.dart';
+import 'widget/TopService.dart';
+import 'widget/CartScreen.dart';
+import 'widget/SplashScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,17 +15,393 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Daftar Buku',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Go Food',
       initialRoute: '/',
       routes: {
-        '/': (context) => BookListPage(),
-        '/add': (context) => AddBookPage(),
-        '/edit': (context) => EditBookPage(bookId: 0),
-        '/detail': (context) => BookDetailPage(bookId: 0),
+        '/': (context) => SplashScreen(),
+        '/home': (context) => HomePage(), // Ganti dengan halaman utama
       },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // App Bar
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60.0),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: AppBar(
+            leading: IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {},
+            ),
+            title: const Center(
+              child: Text(
+                'Go Food', // Judul aplikasi
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.notifications),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: const Icon(Icons.shopping_cart),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => CartScreen()));
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 16.0),
+            // Search bar dan kotak persegi di bawah navbar
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search Here',
+                        prefixIcon: const Icon(Icons.search),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide.none,
+                        ),
+                        fillColor: const Color.fromARGB(255, 183, 218, 177),
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10.0),
+                  // Kotak persegi
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 10, 186, 33),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: const Icon(Icons.filter_alt, color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+
+            // Hero Section dengan Gradient Background di bawah search bar
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              height: 280, // Tinggi Hero section
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 34, 183, 8),
+                    Color.fromARGB(255, 255, 255, 255)
+                  ],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                ),
+              ),
+              child: Row(
+                children: [
+                  // Bagian kiri dengan teks
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Todays Deal',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          '50% OFF',
+                          style: TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          'Temukan Makanan terbaik dengan penawaran menarik. Potongan 50% hanya untuk anda sekarang. Jangan sampai kehabisan!',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black54,
+                          ),
+                        ),
+                        const SizedBox(
+                            height: 16), // Jarak antara teks dan tombol
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromARGB(255, 255, 255, 255),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 12, horizontal: 32),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          child: const Text(
+                            'Pesan Sekarang',
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Color.fromARGB(255, 0, 0, 0)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  SizedBox(
+                    width: 250, // Lebar gambar
+                    height: 200, // Tinggi gambar
+                    child: Image.asset(
+                      'assets/images/banner.png', // Ganti dengan path gambar PNG kamu
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            //Top Seller
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SectionHeader("Top Sellers"),
+                  const SizedBox(height: 12),
+                  Container(
+                    height: 170,
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        TopSeller('assets/images/1.jpg', 'Food1', 4.5),
+                        TopSeller('assets/images/2.jpg', 'Food2', 4.7),
+                        TopSeller('assets/images/3.jpg', 'Food3', 4.5),
+                        TopSeller('assets/images/4.jpg', 'Food4', 4.8),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // Top Services
+            SectionHeader("Top Services"),
+
+            TopService(
+              'assets/images/1.jpg',
+              'assets/images/profil.jpg',
+              4.5,
+            ),
+            TopService(
+              'assets/images/2.jpg',
+              'assets/images/profil.jpg',
+              4.8,
+            ),
+            TopService(
+              'assets/images/3.jpg',
+              'assets/images/profil.jpg',
+              4.7,
+            ),
+
+            SectionHeader("Best Bookings"),
+            SizedBox(height: 12),
+
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              height: 250,
+              width: 370,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 124, 179, 117),
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.2),
+                    blurRadius: 8,
+                    offset: Offset(2, 4),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  // Bagian kiri dengan teks
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Deal of The Day',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          'Free Delivery',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          'Rasakan Makanan Enak Dengan Potongan harga Sebesar 50% hanya untuk anda sekarang.',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.black54,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          '16:34:12',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                          ),
+                        ),
+                        const SizedBox(
+                            height: 16), // Jarak antara teks dan tombol
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 12, horizontal: 32),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          child: const Text(
+                            'Beli Sekarag',
+                            style: TextStyle(fontSize: 14, color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  SizedBox(
+                    width: 190, // Lebar gambar
+                    height: 200, // Tinggi gambar
+                    child: Image.asset(
+                      'assets/images/helm.png', // Ganti dengan path gambar PNG kamu
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 16),
+            BigCard(
+              imagePath: 'assets/images/1.jpg',
+            ),
+            SizedBox(height: 16),
+            BigCard(
+              imagePath: 'assets/images/2.jpg',
+            ),
+            SizedBox(height: 16),
+            BigCard(
+              imagePath: 'assets/images/3.jpg',
+            ),
+
+            // Services List
+            const SizedBox(height: 16),
+            SectionHeader("Produk Terbaru"),
+            const SizedBox(height: 12),
+
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: GridView.count(
+                shrinkWrap:
+                    true, // Agar grid tidak scrollable, karena sudah ada SingleChildScrollView
+                physics:
+                    NeverScrollableScrollPhysics(), // Agar hanya scroll di SingleChildScrollView
+                crossAxisCount: 2, // 2 kolom
+                crossAxisSpacing: 16.0, // Jarak antar kolom
+                mainAxisSpacing: 16.0, // Jarak antar baris
+                childAspectRatio: 0.75, // Rasio tinggi dan lebar card
+                children: [
+                  ProductCard(
+                    imagePath: 'assets/images/1.jpg',
+                    productName: 'Nasi Timbel',
+                    productPrice: 'Rp 150.000',
+                  ),
+                  ProductCard(
+                    imagePath: 'assets/images/2.jpg',
+                    productName: 'Nasi Goreng',
+                    productPrice: 'Rp 120.000',
+                  ),
+                  ProductCard(
+                    imagePath: 'assets/images/3.jpg',
+                    productName: 'Ikan Bakar',
+                    productPrice: 'Rp 200.000',
+                  ),
+                  ProductCard(
+                    imagePath: 'assets/images/4.jpg',
+                    productName: 'Coto Makassar',
+                    productPrice: 'Rp 180.000',
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: 0, // Tetapkan indeks untuk tampilan default
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: "Message",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
     );
   }
 }
